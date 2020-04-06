77c7784d45393f06c477c1df73112635c4ed1f89
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2020-04-06 18:23:37 +0430

    changes to project

---
+++ dbconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+    user          : process.env.NODE_ORACLEDB_USER || "hse",
+  
+    // Get the password from the environment variable
+    // NODE_ORACLEDB_PASSWORD.  The password could also be a hard coded
+    // string (not recommended), or it could be prompted for.
+    // Alternatively use External Authentication so that no password is
+    // needed.
+    password      : process.env.NODE_ORACLEDB_PASSWORD || "hse",
+  
+    // For information on connection strings see:
+    // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
+    connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "172.25.40.114:1522/dev",
+  
+    // Setting externalAuth is optional.  It defaults to false.  See:
+    // https://oracle.github.io/node-oracledb/doc/api.html#extauth
+    externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
+  };
--- oradb.js
+++ oradb.js
@@ -1,5 +1,7 @@
 const oracledb = require('oracledb');
-
+const dbConfig = require('./dbconfig.js');
+const config = require('./config.js');
+ 
 function connectToOracle(){
     const connection = 
         oracledb.getConnection({
@@ -12,16 +14,16 @@ function connectToOracle(){
 }
 
 function runQuery(strQuery){
-    const connection = 
-        oracledb.getConnection({
-            user:"hse",
-            password:"hse",
-            connectString:"172.25.40.114:1522/dev"
-        });
-
-        console.log("successfully connect to oracle");
+        oracledb.getConnection(
+            dbConfig,
+            async function (err,connection){
+                if(err) throw err;
+                const result = await connection.execute(strQuery);
+                return result;
+            }
+        );
 }
 
 module.exports = {
-    connectToOracle
+    connectToOracle,runQuery
 }
