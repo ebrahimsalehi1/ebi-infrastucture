2c904e3b8937091ea7706b6ea667688a6d25ca66
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2020-04-06 18:36:43 +0430

    changes on project

--- config.js
+++ config.js
@@ -4,5 +4,8 @@ var batchSize;    // batch size for fetching rows
 maxRows = 50000;
 batchSize = 1000
 
+sqlFilePath = "./sqlquery/"
+
 exports.maxRows = maxRows;
 exports.batchSize = batchSize;
+exports.sqlFilePath = sqlFilePath
